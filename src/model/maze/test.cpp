fcf83da668cbbf4044b8ac8f6be9489c21237283
rubiezzy <rubiezzy@student.21-school.ru>
2022-11-18 15:21:11 +0400

    added tests directory

--- src/model/maze/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "maze.h"
-
-int main() {
-    s21::Maze maze("generated_maze.txt");
-    maze.outputMaze();
-//    std::ofstream file("generated_maze.txt");
-//    file << maze;
-//    file.close();
-}
