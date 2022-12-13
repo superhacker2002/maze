d38b2dd43c7b51e79d5b22a43c2543de4d03a351
superhacker2002 <margo.gamaleeva@yandex.ru>
2022-12-13 14:04:51 +0300

    new

--- src/model/maze/tests.cc
+++ src/model/maze/tests.cc
@@ -2,9 +2,9 @@
 #include "answer/maze_answer.h"
 
 int main() {
-    s21::Maze maze("maze1.txt");
+    s21::Maze maze("../../../datasets/maze_files/maze2.txt");
     maze.outputMaze();
     s21::Coordinates start = {0, 0};
-    s21::Coordinates end = {4, 4};
+    s21::Coordinates end = {9, 9};
     s21::getMazeAnswer(maze, start, end);
 }
