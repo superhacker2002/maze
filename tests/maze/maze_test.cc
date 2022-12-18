af9c4499da649977555a27ea1b397a4b2325fc6d
superhacker2002 <margo.gamaleeva@yandex.ru>
2022-12-18 19:35:21 +0300

    added tests for answer but something went wrong

--- tests/maze/maze_test.cc
+++ tests/maze/maze_test.cc
@@ -1,9 +1,11 @@
 #include "gtest/gtest.h"
 #include "maze_test_class.h"
+#include "../../src/model/maze/answer/maze_answer.h"
 
 MazeTest test_maze(5, 5);
+s21::Maze test_answer_maze("datasets/maze_files/maze1.txt");
 
-TEST(maze_tests, fill_empty_values) {
+TEST(maze_generation_tests, fill_empty_values) {
     test_maze.testFillEmptyValues();
     std::vector<int> result = test_maze.getSideLine();
     EXPECT_EQ(result.size(), 5);
@@ -12,7 +14,7 @@ TEST(maze_tests, fill_empty_values) {
     }
 }
 
-TEST(maze_tests, assign_unique_set) {
+TEST(maze_generation_tests, assign_unique_set) {
     test_maze.testAssignUniqueSet();
     std::vector<int> result = test_maze.getSideLine();
     EXPECT_EQ(result.size(), 5);
@@ -22,7 +24,7 @@ TEST(maze_tests, assign_unique_set) {
     }
 }
 
-TEST(maze_tests, add_right_walls) {
+TEST(maze_generation_tests, add_right_walls) {
     test_maze.testAddRightWalls(0);
     std::vector<int> result = test_maze.getSideLine();
     EXPECT_EQ(result.size(), 5);
@@ -37,7 +39,7 @@ TEST(maze_tests, add_right_walls) {
     }
 }
 
-TEST(maze_tests, add_bottom_walls) {
+TEST(maze_generation_tests, add_bottom_walls) {
     test_maze.testAddBottomWalls(0);
     std::vector<int> result = test_maze.getSideLine();
     EXPECT_EQ(result.size(), 5);
@@ -57,7 +59,7 @@ TEST(maze_tests, add_bottom_walls) {
     }
 }
 
-TEST(maze_tests, prepare_new_line) {
+TEST(maze_generation_tests, prepare_new_line) {
     std::vector<bool> prev_line;
     int cols = test_maze.GetCols();
     for (int col = 0; col < cols; ++col) {
@@ -77,13 +79,48 @@ TEST(maze_tests, prepare_new_line) {
     }
 }
 
-TEST(maze_tests, check_end_line) {
+TEST(maze_generation_tests, check_end_line) {
     test_maze.testCheckEndLine();
     int row = test_maze.GetRows() - 1;
     int cols = test_maze.GetCols();
     for (int col = 0; col < cols; ++col) {
         EXPECT_TRUE(test_maze.GetValue(row, col).bottom_wall);
     }
+}
+
+TEST(maze_answer_tests, get_answer_invalid) {
+    s21::Maze test_answer_maze("datasets/maze_files/maze1.txt");
+    EXPECT_ANY_THROW(s21::getMazeAnswer(test_answer_maze, {0, 0}, {10, 10}));
+    EXPECT_ANY_THROW(s21::getMazeAnswer(test_answer_maze, {10, 10}, {0, 0}));
+    EXPECT_ANY_THROW(s21::getMazeAnswer(test_answer_maze, {0, 0}, {-1, -1}));
+    EXPECT_ANY_THROW(s21::getMazeAnswer(test_answer_maze, {-1, -1}, {0, 0}));
+}
+
+TEST(maze_answer_tests, get_answer_usual_1) {
+    std::vector<int> correct_answer = {s21::RIGHT, s21::RIGHT, s21::RIGHT, 
+                                        s21::DOWN, s21::DOWN, s21::LEFT,
+                                        s21::DOWN, s21::RIGHT};
+    auto it = correct_answer.begin();
+    std::vector<int> result = s21::getMazeAnswer(test_answer_maze, {0, 0}, {3, 3});
+    for (auto& cell : result) {
+        EXPECT_EQ(cell, *it);
+        it++;
+    }
+}
+
+TEST(maze_answer_tests, get_answer_usual_2) {
+    // std::vector<int> correct_answer = {s21::RIGHT, s21::RIGHT, s21::RIGHT, 
+    //                                     s21::DOWN, s21::DOWN, s21::LEFT,
+    //                                     s21::DOWN, s21::RIGHT};
+    // auto it = correct_answer.begin();
+    std::vector<int> result = s21::getMazeAnswer(test_answer_maze, {0, 2}, {0, 3});
+    std::cout << result.size();
+    for (auto& cell : result) {
+        // EXPECT_EQ(cell, *it);
+        // it++;
+        std::cout << cell << " ";
+    }
+    std::cout << std::endl;
 }
 
 int main(int argc, char **argv) {
